38eb553a7504914156345f94a631375bac4ff0b7
Wecodex <67681262+wecodexoficial@users.noreply.github.com>
2025-04-17 14:54:10 -0600

    Update index.php

--- Archivos PHP/index.php
+++ Archivos PHP/index.php
@@ -1,19 +1,5 @@
 <?php
-/**
- * index.php
- * Created By: Josué Saúl Martínez
- * Date: 26/04/2017
- * Time: 21:53
- *
- * This is the version 2.1 of Euromundo.com
- * It consists of a set of components for the creation of a unified core,
- * under the HMVC software architecture,
- * which provides the possibility to create modules efficiently and quickly.
- * This file is the trigger of the instructions and load order of the elements.
- * The core consists of security modules, routes, validations, mail,
- * database connections of dynamic model, master controller,
- * image processor and exquisite error handling.
- */
+ 
 date_default_timezone_set('America/Mexico_City');
 
 /**
